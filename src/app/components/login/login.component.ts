027e8fdb7d677182d3abea1e1aa2e36db9f1b1f5
BondarencoM <bondarenco.mihail@gmail.com>
2019-05-28 19:58:05 +0300

    feat(login) implement redirect to all-places page after login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { AlertsComponent } from '../includes/alerts/alerts.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,8 @@ import { AlertsComponent } from '../includes/alerts/alerts.component';
 export class LoginComponent implements OnInit {
   public loginUsername
   public loginPassword
-  constructor(private auth: AuthenticationService) { }
+  constructor(private auth: AuthenticationService,
+    private router: Router) { }
 
   ngOnInit() {
   }
@@ -19,8 +21,8 @@ export class LoginComponent implements OnInit {
       this.auth.obtainAccessToken(this.loginUsername, this.loginPassword,this.ifSucces,this.ifFail)
   }
 
-  ifSucces(data){
-    alert("success");
+  ifSucces = (data) => {
+    this.router.navigate(['/all-places'])//.then(() => AlertsComponent.displaySuccess("Log in successfull")) // disabled until dismissable alerts are available
   }
 
   ifFail(error){
