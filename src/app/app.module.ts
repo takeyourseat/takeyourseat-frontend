91f494c2e0f7bd0c4c26728c8e8551a791fb1cf1
cristiduluta <cristi.duluta@outlook.com>
2019-05-30 14:47:52 +0300

    feat(*) add empty placetable component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AllplacesComponent } from './components/allplaces/allplaces.component';
+import { PlacetableComponent } from './components/utils/placetable/placetable.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AllplacesComponent
+    AllplacesComponent,
+    PlacetableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/utils/placetable/placetable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-placetable',
+  templateUrl: './placetable.component.html',
+  styleUrls: ['./placetable.component.css']
+})
+export class PlacetableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
