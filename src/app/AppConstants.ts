321f5598cff0e9a4904a3517ee19bc972ffdd8a3
Bondarenco <mbondare@techteam.com>
2019-05-30 19:58:56 +0300

    fix(authenticate) update authentication server port

--- src/app/AppConstants.ts
+++ src/app/AppConstants.ts
@@ -1,5 +1,5 @@
 export class AppConstants {
     public static CLIENT_ID = (): string => 'angular-app'
     public static CLIENT_SECRET = (): string => 'yKUiY5q5kQLmvkkCjgAs4WqSKNBQ5JDM1LOYshl3'
-    public static AUTH_GET_TOKEN_URL = (): string => 'http://localhost:9191/oauth/token'
+    public static AUTH_GET_TOKEN_URL = (): string => 'http://localhost:8083/oauth/token'
 }
