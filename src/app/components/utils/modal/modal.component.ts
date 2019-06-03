17cef2978609d2314e3ac18b3b6595b5cdcfb8c3
cristiduluta <cristi.duluta@outlook.com>
2019-06-03 09:49:45 +0300

    feat(*) add empty modal component

---
+++ src/app/components/utils/modal/modal.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Office } from 'src/app/model/Office';
+import { Place } from 'src/app/model/Place';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
