7a5f9fbe2430f71393b8c0594fbcd3a603f26076
cristiduluta <cristi.duluta@outlook.com>
2019-06-04 11:08:04 +0300

    feat(service) add getfloors(),getoffice() and getofficesbyfloor() methods for office service

--- src/app/service/office.service.ts
+++ src/app/service/office.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Office } from 'src/app/model/Office';
+import { HttpClient } from '@angular/common/http';
+
+const endpoint = 'http://localhost:8084/api/';
 
 @Injectable({
   providedIn: 'root'
@@ -6,7 +10,18 @@ import { Injectable } from '@angular/core';
 export class OfficeService {
 
   constructor(
-
+    private http: HttpClient
   ) { }
 
+  getFloors() {
+    return this.http.get<[]>(endpoint + 'floors');
+  }
+
+  getOffices() {
+    return this.http.get<Office[]>(endpoint + 'offices/');
+  }
+
+  getOfficesByFloor(floor) {
+    return this.http.get<Office[]>(endpoint + `offices?floor=${floor}`);
+  }
 }
