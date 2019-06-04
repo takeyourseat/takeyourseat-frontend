c99fbaed62e008df996f04ab59b8118a04caf6cf
cristiduluta <cristi.duluta@outlook.com>
2019-06-04 11:12:33 +0300

    feat(service) add getplacesbyofficenumber() and getplacesbyofficeid() methods for place service

--- src/app/service/place.service.ts
+++ src/app/service/place.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Place } from 'src/app/model/Place';
+import { HttpClient } from '@angular/common/http';
+
+const endpoint = 'http://localhost:8084/api/';
 
 @Injectable({
   providedIn: 'root'
@@ -6,7 +10,14 @@ import { Injectable } from '@angular/core';
 export class PlaceService {
 
   constructor(
-
+    private http: HttpClient
   ) { }
 
+  getPlacesByOfficeNumber(officenum: number) {
+    return this.http.get<Place[]>(endpoint + `offices/${officenum}/places`);
+  }
+
+  getPlacesByOfficeId(officeId: number) {
+    return this.http.get<Place[]>(endpoint + `offices/${officeId}/places`);
+  }
 }
