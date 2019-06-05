eaa5492f3f330081803429a8df6d50cf3234a772
cristiduluta <cristi.duluta@outlook.com>
2019-06-05 09:24:27 +0300

    feature(component) implement showplace method  in placetable component

--- src/app/components/utils/placetable/placetable.component.ts
+++ src/app/components/utils/placetable/placetable.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Office } from 'src/app/model/Office';
+import { PlaceService } from 'src/app/service/place.service';
+import { Place } from 'src/app/model/Place';
+
 
 @Component({
   selector: 'app-placetable',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlacetableComponent implements OnInit {
 
+  private schema: any[][];
+  private _places: Place[] = [];
+  private _selectedOffice: Office;
+
+  @Input()
+  set places(places: Place[]) {
+    this._places = places;
+    this._selectedOffice = places[0].office;
+    this.showPlaces();
+  }
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  showPlaces() {
+    this.schema = [[]];
+
+    for (let i = 0; i < this._selectedOffice.sizeX; i++) {
+      this.schema[i] = [];
+      for (let j = 0; j < this._selectedOffice.sizeY; j++) {
+        this.schema[i][j] = {};
+      }
+    }
+
+    this._places.forEach(place => {
+      this.schema[place.coordinateX][place.coordinateY].isPlace = true;
+      this.schema[place.coordinateX][place.coordinateY].isAvailable = !place.userId;
+    })
+  }
 }
