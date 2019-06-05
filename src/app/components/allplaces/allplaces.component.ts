a4befd7fc4ffa6950f62c847d4433d9e1f85cc25
cristiduluta <cristi.duluta@outlook.com>
2019-06-05 09:28:03 +0300

    feature(component) add getFloors(), getOffices(), and selectOffice() methods for allplaces component

--- src/app/components/allplaces/allplaces.component.ts
+++ src/app/components/allplaces/allplaces.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { OfficeService } from 'src/app/service/office.service';
+import { PlaceService } from 'src/app/service/place.service';
+import { Office } from 'src/app/model/Office';
+import { Place } from 'src/app/model/Place';
+import { PlacetableComponent } from '../utils/placetable/placetable.component';
 
 @Component({
   selector: 'app-allplaces',
   templateUrl: './allplaces.component.html',
-  styleUrls: ['./allplaces.component.css']
+  styleUrls: ['./allplaces.component.css'],
+  providers: [PlacetableComponent],
 })
 export class AllplacesComponent implements OnInit {
 
-  constructor() { }
+  offices: Office[];
+  places: Place[];
+  floors: [];
+  selectedOffice: Office;
+
+  constructor(
+    private officeService: OfficeService,
+    private placeService: PlaceService,
+  ) { }
 
   ngOnInit() {
+    this.getFloors();
   }
 
+  getFloors() {
+    this.officeService.getFloors().subscribe(
+      response => {
+        this.floors = response;
+      }
+    )
+  }
+
+  getOffices(id) {
+    this.officeService.getOfficesByFloor(id).subscribe(
+      response => {
+        this.offices = response;
+      }
+    )
+  }
+
+  selectOffice(office: Office) {
+    this.selectedOffice = office
+    this.placeService.getPlacesByOfficeId(office.id).subscribe(
+      response => {
+        this.places = response;
+      }
+    )
+  }
 }
