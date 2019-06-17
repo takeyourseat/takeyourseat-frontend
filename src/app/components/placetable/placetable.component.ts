017ce9dc1ccd50d668d6d66f8942889bdb3b42a1
cristiduluta <cristi.duluta@outlook.com>
2019-06-17 11:01:59 +0300

    feat(component) add data-toggle modal for placemarker component

--- src/app/components/placetable/placetable.component.ts
+++ src/app/components/placetable/placetable.component.ts
@@ -1,7 +1,6 @@
-import {Component, OnInit, Input} from '@angular/core'
-import {Office} from 'src/app/model/Office'
-import {PlaceService} from 'src/app/service/place.service'
-import {Place} from 'src/app/model/Place'
+import { Component, OnInit, Input } from '@angular/core';
+import { Office } from 'src/app/model/Office';
+import { Place } from 'src/app/model/Place';
 
 @Component({
   selector: 'app-placetable',
@@ -9,46 +8,45 @@ import {Place} from 'src/app/model/Place'
   styleUrls: ['./placetable.component.css'],
 })
 export class PlacetableComponent implements OnInit {
-  private schema: any[][]
-  private _places: Place[] = []
-  private _selectedOffice: Office
-  private _selectedPlace: Place
+  private schema: any[][];
+  private _places: Place[] = [];
+  private _selectedOffice: Office;
+  private selectedPlace: Place;
 
   @Input()
   set places(places: Place[]) {
-    this._places = places
-    this.showPlaces()
+    this._places = places;
+    this.showPlaces();
   }
 
   @Input()
   set selectedOffice(selectedOffice: Office) {
-    this._selectedOffice = selectedOffice
+    this._selectedOffice = selectedOffice;
   }
 
-  constructor() {}
+  constructor() {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
   showPlaces() {
-    this.schema = [[]]
+    this.schema = [[]];
     if (!this._selectedOffice) {
-      return
+      return;
     }
     for (let i = 0; i < this._selectedOffice.sizeX; i++) {
-      this.schema[i] = []
+      this.schema[i] = [];
       for (let j = 0; j < this._selectedOffice.sizeY; j++) {
-        this.schema[i][j] = {}
+        this.schema[i][j] = {};
       }
     }
     this._places.forEach(place => {
-      this.schema[place.coordinateX][place.coordinateY].isPlace = true
-      this.schema[place.coordinateX][
-        place.coordinateY
-      ].isAvailable = !place.userId
-    })
+      this.schema[place.coordinateX][place.coordinateY] = { ...place, isPlace: true, isAvailable: !place.userId };
+    });
   }
 
   selectPlace(place: Place) {
-    this._selectedPlace = place
+    this.selectedPlace = place;
   }
 }
