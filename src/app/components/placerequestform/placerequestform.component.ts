1fd5284fc3ee7784d93be624bf81dda211bd8e72
cristiduluta <cristi.duluta@outlook.com>
2019-06-17 11:28:25 +0300

    feat(component) add placerequestform component

---
+++ src/app/components/placerequestform/placerequestform.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Place} from '../../model/Place';
+
+@Component({
+  selector: 'app-placerequestform',
+  templateUrl: './placerequestform.component.html',
+  styleUrls: ['./placerequestform.component.css']
+})
+export class PlacerequestformComponent implements OnInit {
+
+  private _selectedPlace: Place;
+
+  @Input()
+  set selectedPlace(place: Place) {
+    this._selectedPlace = place;
+  }
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
