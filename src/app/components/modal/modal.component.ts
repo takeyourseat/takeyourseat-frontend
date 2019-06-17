2c3d8ba4e086c311142901875e8ba719375cfdf2
cristiduluta <cristi.duluta@outlook.com>
2019-06-17 10:39:22 +0300

    feat(component) import bootstrap modal and implement its functionality

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Office } from 'src/app/model/Office';
 import { Place } from 'src/app/model/Place';
+import 'bootstrap/js/dist/modal';
 
 @Component({
   selector: 'app-modal',
@@ -8,11 +8,11 @@ import { Place } from 'src/app/model/Place';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
-  private _selectedPlace: Place
+  private _selectedPlace: Place;
 
   @Input()
   set selectedPlace(place: Place) {
-    this._selectedPlace = place
+    this._selectedPlace = place;
   }
 
   constructor() { }
