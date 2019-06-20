b082bd3d7b051b55db642a376decf858f3991dfd
Eric <gutsueric@gmail.com>
2019-06-20 15:57:51 +0300

    feature(modal.component) add method createPlaceRequest

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Place } from 'src/app/model/Place';
 import 'bootstrap/js/dist/modal';
+import {PlacerequestService} from '../../service/placerequest.service';
+import {PlaceRequests} from '../../model/PlaceRequests';
 
 @Component({
   selector: 'app-modal',
@@ -9,15 +11,26 @@ import 'bootstrap/js/dist/modal';
 })
 export class ModalComponent implements OnInit {
   private _selectedPlace: Place;
+  private placeRequest: PlaceRequests;
 
   @Input()
   set selectedPlace(place: Place) {
     this._selectedPlace = place;
   }
 
-  constructor() { }
+  constructor(
+    private placeRequestService: PlacerequestService
+
+  ) { }
 
   ngOnInit() {
   }
 
+  createPlaceRequest() {
+    this.placeRequestService.createPlaceRequest(this._selectedPlace.id).subscribe(
+      response => {
+        this.placeRequest = response;
+      }
+    );
+  }
 }
