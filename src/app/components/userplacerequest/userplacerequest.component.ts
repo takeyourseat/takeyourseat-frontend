946d31d9f0030aa38576990959fea8dcb4b6bc40
Eric <gutsueric@gmail.com>
2019-06-24 17:32:11 +0300

    feature(userplacerequest) add readPlaces method

--- src/app/components/userplacerequest/userplacerequest.component.ts
+++ src/app/components/userplacerequest/userplacerequest.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {User} from '../../model/User';
+import {Component, Input, OnInit} from '@angular/core';
 import {PlaceRequests} from '../../model/PlaceRequests';
 import {UserService} from '../../service/user.service';
 import {PlacerequestService} from '../../service/placerequest.service';
+import {PlaceService} from '../../service/place.service';
 
 @Component({
   selector: 'app-userplacerequest',
@@ -15,20 +15,32 @@ export class UserplacerequestComponent implements OnInit {
 
   constructor(
     private userService: UserService,
-    private placeRequestService: PlacerequestService
+    private placeRequestService: PlacerequestService,
+    private placeService: PlaceService
   ) {
   }
 
   ngOnInit() {
-    this.loadPlaceRequestsByUserId(4);
+    this.loadPlaceRequestsByUserId(5);
   }
 
   loadPlaceRequestsByUserId(userId: number) {
     this.placeRequestService.getPlaceRequestsByUserId(userId).subscribe(
       response => {
         this.placeRequests = response;
+        this.readPlaces(this.placeRequests);
       }
     );
+  }
+
+  readPlaces(placeRequests: PlaceRequests[]) {
+    for (const placeRequest of placeRequests) {
+      this.placeService.getPlaceById(placeRequest.placeId).subscribe(
+        response => {
+          placeRequest.place = response;
+        }
+      );
+    }
   }
 
 }
