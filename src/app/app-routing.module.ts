d112ff4555e8ca8ee9fe0c7008d8d96b717dbab3
Eric <gutsueric@gmail.com>
2019-06-24 13:15:05 +0300

    feature(app-routing) routing userplacerequest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {AllplacesComponent} from './components/allplaces/allplaces.component';
 import {RequestedplaceComponent} from './components/requestedplace/requestedplace.component';
+import {UserplacerequestComponent} from './components/userplacerequest/userplacerequest.component';
 
 const routes: Routes = [
   {path: 'places', component: AllplacesComponent},
-  {path: 'requestedplace', component: RequestedplaceComponent}
+  {path: 'requestedplace', component: RequestedplaceComponent},
+  {path: 'userplacerequest', component: UserplacerequestComponent}
 ];
 
 @NgModule({
