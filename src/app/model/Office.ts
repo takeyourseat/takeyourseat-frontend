c7562c926ef7665db8594998b2403203ce1bd394
Eric <gutsueric@gmail.com>
2019-06-24 17:32:35 +0300

    refactor(Office) add office number field

--- src/app/model/Office.ts
+++ src/app/model/Office.ts
@@ -4,11 +4,13 @@ export class Office {
   sizeY: number;
   floor: number;
   description: string;
+  number: number;
 
-  constructor(id: number, sizeX: number, sizeY: number, description: string) {
+  constructor(id: number, sizeX: number, sizeY: number, description: string, officeNumber: number) {
     this.id = id;
     this.sizeX = sizeX;
     this.sizeY = sizeY;
     this.description = description;
+    this.number = officeNumber;
   }
 }
