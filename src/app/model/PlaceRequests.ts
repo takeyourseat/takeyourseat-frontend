2cee54ceed3f134ca9661ccb7107ba1e1e1da593
Eric <gutsueric@gmail.com>
2019-06-24 17:33:05 +0300

    feature(PlaceRequests) add Place and User fields

--- src/app/model/PlaceRequests.ts
+++ src/app/model/PlaceRequests.ts
@@ -1,15 +1,22 @@
+import {Place} from './Place';
+import {User} from './User';
+
 export class PlaceRequests {
   id: number;
   approved: boolean;
   managerId: number;
   placeId: number;
-  userId: number
+  userId: number;
+  place: Place;
+  user: User;
 
-  constructor(id: number, approved: boolean, managerId: number, placeId: number, userId: number) {
+  constructor(id: number, approved: boolean, managerId: number, placeId: number, userId: number, place: Place, user: User) {
     this.id = id;
     this.approved = approved;
     this.managerId = managerId;
     this.placeId = placeId;
     this.userId = userId;
+    this.place = place;
+    this.user = user;
   }
 }
