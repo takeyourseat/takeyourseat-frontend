15b287bd2936176ccc81010c199f4c8dc42ccb53
Eric <gutsueric@gmail.com>
2019-06-27 14:40:28 +0300

    refactor(authentication.service) fix imports

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { AppConstants } from '../AppConstants';
+import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {AppConstants} from '../AppConstants';
 
 @Injectable({
   providedIn: 'root'
