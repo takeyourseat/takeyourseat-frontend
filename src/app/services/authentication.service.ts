e817678caec72ed3a8b73979f22a73f525afe7fd
Eric <gutsueric@gmail.com>
2019-06-27 14:41:14 +0300

    refactor(authentication.service) apply TSLint convention

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -11,52 +11,54 @@ export class AuthenticationService {
   constructor(
     private router: Router,
     private http: HttpClient
-  ) { }
+  ) {
+  }
 
   obtainAccessToken(loginUsername, loginPassword, ifSucces: (data) => void, ifFail: (error) => void) {
     let headers = new HttpHeaders()
       .append('Content-type', 'application/x-www-form-urlencoded; charset=utf-8')
-      .append('Authorization', 'Basic ' + btoa(AppConstants.CLIENT_ID() + ":" + AppConstants.CLIENT_SECRET()));
+      .append('Authorization', 'Basic ' + btoa(AppConstants.CLIENT_ID() + ':' + AppConstants.CLIENT_SECRET()));
 
-    var credentials = ""
-      + "username=" + loginUsername
-      + "&password=" + loginPassword
-      + "&grant_type=password"
+    var credentials = ''
+      + 'username=' + loginUsername
+      + '&password=' + loginPassword
+      + '&grant_type=password';
 
-    this.http.post(AppConstants.AUTH_GET_TOKEN_URL(), credentials, { headers: headers })
+    this.http.post(AppConstants.AUTH_GET_TOKEN_URL(), credentials, {headers: headers})
       .subscribe(
         data => {
-          sessionStorage.setItem("access_token", data['access_token'])
-          sessionStorage.setItem("username", loginUsername)
-          ifSucces(data)
+          sessionStorage.setItem('access_token', data['access_token']);
+          sessionStorage.setItem('username', loginUsername);
+          ifSucces(data);
         },
         error => {
-          ifFail(error)
+          ifFail(error);
         });
   }
 
   /** Ask before Using!! */
-  getToken(): string{
+  getToken(): string {
     return sessionStorage.getItem('access_token');
   }
 
-  getUserName():string{
+  getUserName(): string {
     return sessionStorage.getItem('username');
   }
 
-  isUserLoggedIn():boolean{
-    if(!sessionStorage.getItem('access_token'))
-      return false
-    else if(!sessionStorage.getItem('username'))
-      return false
-    else 
-      return true
+  isUserLoggedIn(): boolean {
+    if (!sessionStorage.getItem('access_token')) {
+      return false;
+    } else if (!sessionStorage.getItem('username')) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
   logOut() {
-    sessionStorage.removeItem('access_token')
-    sessionStorage.removeItem('username')
-    
+    sessionStorage.removeItem('access_token');
+    sessionStorage.removeItem('username');
+
   }
-  
+
 }
