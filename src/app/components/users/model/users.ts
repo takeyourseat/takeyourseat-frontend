fd653a15ad4e44a8fa507a675bbf5fc7d89babf5
IgorVacarencov <49471246+IgorVacarencov@users.noreply.github.com>
2019-07-02 09:50:13 +0300

    modified front and added popup window to edit button

--- src/app/components/users/model/users.ts
+++ src/app/components/users/model/users.ts
@@ -1,34 +1,36 @@
 export interface AllUsers {
-    id:string;
+  id: string;
+  fName: string;
+  lName: string;
+  email: string;
+  username: string;
+  jobTitle: string;
+  manager: {
     fName: string;
-    lName: string;
-    email: string;
-    username: string;
-    jobTitle: string;
-    manager: {
-      fName: string;
-    };
-    role: {
-      name: string;
-    };
-  }
-  
-  export interface OneUser {
-    id:string;
-    fName: string;
-    lName: string;
-    email: string;
-    username: string;
-    jobTitle: string;
-    manager: {
-      fName: string;
-    };
-    role: {
-      name: string;
-    };
-  }
-  
-  export interface Role {
-    id: string;
+  };
+  role: {
     name: string;
-  }
+  };
+
+}
+
+export interface OneUser {
+  id: string;
+  fName: string;
+  lName: string;
+  email: string;
+  username: string;
+  jobTitle: string;
+  manager: {
+    fName: string;
+  };
+  role: {
+    name: string;
+  };
+
+}
+
+export interface Role {
+  id: string;
+  name: string;
+}
