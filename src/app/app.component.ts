ebe164eec7c29455cf4f839f2965d65906ba3dd6
Gutu <egutu2@techteam.com>
2019-07-09 18:02:32 +0300

    feature(login.component) add request for permission to push notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
-import {SwPush} from '@angular/service-worker';
-import {PlacerequestService} from './services/placerequest.service';
+
 
 @Component({
   selector: 'app-root',
@@ -10,19 +9,5 @@ import {PlacerequestService} from './services/placerequest.service';
 export class AppComponent {
   title = 'takeyourseat';
 
-  readonly VAPID_PUBLIC_KEY = 'BLBx-hf2WrL2qEa0qKb-aCJbcxEvyn62GDTyyP9KTS5K7ZL0K7TfmOKSPqp8vQF0DaG8hpSBknz_x3qf5F4iEFo';
 
-  constructor(
-    private swPush: SwPush,
-    private placerequestService: PlacerequestService) {
-  }
-
-  subscribeToNotifications() {
-
-    this.swPush.requestSubscription({
-      serverPublicKey: this.VAPID_PUBLIC_KEY
-    })
-      .then(sub => this.placerequestService.getPlaceRequestsByUsername('csava').subscribe())
-      .catch(err => console.error('Could not subscribe to notifications', err));
-  }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from 'src/app/services/authentication.service';
-import { AlertsComponent } from '../alerts/alerts.component';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from 'src/app/services/authentication.service';
+import {AlertsComponent} from '../alerts/alerts.component';
+import {Router} from '@angular/router';
+import {SwPush} from '@angular/service-worker';
+import {PlacerequestService} from '../../services/placerequest.service';
 
 @Component({
   selector: 'app-login',
@@ -9,24 +11,43 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  public loginUsername
-  public loginPassword
+  public loginUsername;
+  public loginPassword;
+  readonly VAPID_PUBLIC_KEY = 'BLBx-hf2WrL2qEa0qKb-aCJbcxEvyn62GDTyyP9KTS5K7ZL0K7TfmOKSPqp8vQF0DaG8hpSBknz_x3qf5F4iEFo';
+
   constructor(private auth: AuthenticationService,
-    private router: Router) { }
+              private router: Router,
+              private swPush: SwPush,
+              private placerequestService: PlacerequestService) {
+  }
+
+  subscribeToNotifications() {
+
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    })
+      .then(sub => this.placerequestService.getPlaceRequestsByUsername('csava').subscribe())
+      .catch(err => console.error('Could not subscribe to notifications', err));
+  }
 
   ngOnInit() {
   }
 
   login() {
-      this.auth.obtainAccessToken(this.loginUsername, this.loginPassword,this.ifSucces,this.ifFail)
+    this.auth.obtainAccessToken(this.loginUsername, this.loginPassword, this.ifSucces, this.ifFail);
   }
 
   ifSucces = (data) => {
-    this.router.navigate(['/places'])
-  }
+    this.router.navigate(['/places']);
+    this.subscribeToNotifications();
+  };
 
-  ifFail(error){
-    AlertsComponent.display("danger","Unable to Log in, please try again")
+  ifFail(error) {
+    AlertsComponent.display('danger', 'Unable to Log in, please try again');
   }
 
 }
+
+
+
+
