55b395aa64685d4616c38cbd567c03f1cef1a01b
Eric <gutsueric@gmail.com>
2019-07-12 15:28:30 +0300

    feature(NushNotification) implement notification API

--- src/app/components/notification-sidebar/notification-sidebar.component.ts
+++ src/app/components/notification-sidebar/notification-sidebar.component.ts
@@ -12,37 +12,27 @@ export class NotificationSidebarComponent implements OnInit {
   notifications: PushNotification[] = [];
 
   constructor(push: SwPush) {
+
+    push.notificationClicks.subscribe(
+      (arg) => {
+        console.log('notification clicked ' + arg.notification.data.url);
+      }
+    );
+
     push.messages.subscribe(msg => {
+      console.log(msg);
       this.notifications.unshift(msg as PushNotification);
     });
 
     const key = 'BIo4B1bsWsS3fDQZJjFo3k_M9C5sMm929H5EJMbqcYicjCiseaYeCDsE6dIB5NNw4u6rlW8YUWhs-evYAwa2mOM';
     const privateKey = 'dw1-Fz9_bD1aX9OAZ8uRt8c5p-CNNczirkGBiMYTUVM';
-    push.requestSubscription({serverPublicKey: key})
+    push.requestSubscription(
+      {serverPublicKey: key})
       .then(pushSubscription => {
         console.log(pushSubscription.toJSON());
       });
   }
 
   ngOnInit() {
-    this.notifications.unshift(
-      {
-        category: 'Place',
-        notification: {
-          title: 'Here is title',
-          body: 'mha body is nobody body',
-          image: '../../../assets/icons/icon-72x72.png'
-        },
-      });
-    this.notifications.unshift({
-        category: 'Place',
-        notification: {
-          title: 'Hey oh',
-          body: 'listen what I say oh',
-          image: '../../../assets/icons/icon-72x72.png'
-        },
-      },
-    );
   }
-
 }
